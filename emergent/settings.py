695c9f202f827ffb1b16cfd1aaec0a1e877b1f31
Mark Vasilkov <mvasilkov@gmail.com>
2013-08-10 17:17:52 +0300

    Clean up settings a bit. Can locate its own path

--- emergent/settings.py
+++ emergent/settings.py
@@ -1,13 +1,13 @@
 # Django settings for emergent_dev project.
 import os
-from my_settings import PROJECT_PATH
+try:
+    from my_settings import PROJECT_PATH
+except ImportError:
+    import os.path as _p
+    PROJECT_PATH = _p.dirname(_p.dirname(_p.abspath(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
 
 MANAGERS = ADMINS
 
@@ -129,32 +129,3 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'django_extensions'
 )
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
