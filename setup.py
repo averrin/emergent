82e87f4d9b1b651f93842f3b8cb4dbc792059140
Averrin <averrin@gmail.com>
2013-08-11 18:47:07 +0000

    django to travis 3th try #21

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@
 
 from setuptools import setup, find_packages
 
-setup(name='Project Emergent',
-      version='0.1',
-      description='Coding anarchy',
-      author='*',
-      author_email='averrin@gmail.com',
-      url='https://github.com/averrin/emergent/',
-      packages=find_packages(),
-      license='License :: MIT',
+setup(
+    name='Project Emergent',
+    version='0.1',
+    description='Coding anarchy',
+    author='*',
+    author_email='averrin@gmail.com',
+    url='https://github.com/averrin/emergent/',
+    packages=find_packages(),
+    license='License :: MIT',
 
-      # Enable django-setuptest
-      test_suite='setuptest.setuptest.SetupTestSuite',
-      tests_require=(
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    tests_require=(
         'django-setuptest',
-        # Required by django-setuptools on Python 2.6
         'argparse'
-      ),
+    ),
 )
